8cda20b1b6906ecfe63f6b431cdbdee6cb2773c6
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-08-05 19:11:38 +0530

    Created retrofit object

--- app/src/main/java/pramod/com/coronaupdates/MainActivity.java
+++ app/src/main/java/pramod/com/coronaupdates/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.covid19api.com/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+
+        CoronaInterface ci = retrofit.create(CoronaInterface.class);
     }
 }
